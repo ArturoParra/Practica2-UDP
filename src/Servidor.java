2bf294fe99f8544fb9b318702f5bb3e4a9d3a491
ArturoParra <aparrar1900@alumno.ipn.mx>
2025-04-01 07:13:08 -0600

    Aplicación terminada con envío de archivos en paquetes y acuses

--- src/Servidor.java
+++ src/Servidor.java
@@ -1,45 +1,58 @@
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.io.*;
+import java.net.*;
 
 public class Servidor {
+    private static final int PORT = 9876;
+    private static final int PACKET_SIZE = 10;
+    private static final String OUTPUT_FILE = "received_file.txt";
 
-    public static void main(String[] args) throws SocketException {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket(PORT);
+        int expectedSeq = 0;
+        boolean lastPacketReceived = false;
+        FileOutputStream fos = new FileOutputStream(OUTPUT_FILE);
 
-        final int PORT = 5000;
-        byte[] buffer = new byte[1024];
+        while (!lastPacketReceived) {
+            byte[] receiveData = new byte[PACKET_SIZE + 4];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            socket.receive(receivePacket);
 
-        try(DatagramSocket socket = new DatagramSocket(PORT)) {
-            System.out.println("Servidor escuchando en el puerto " + PORT);
+            int seqNum = ((receiveData[0] & 0xFF) << 8) | (receiveData[1] & 0xFF);
+            boolean isLast = receiveData[2] == 1;
+            byte checksum = receiveData[3];
+            byte[] fileData = new byte[receivePacket.getLength() - 4];
+            System.arraycopy(receiveData, 4, fileData, 0, fileData.length);
 
-            while(true) {
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-                socket.receive(packet);
-                System.out.println("Recibiendo información del cliente");
-                String mensaje = new String(packet.getData());
-                System.out.println("Mensaje recibido: " + mensaje);
-
-                int puertoCliente = packet.getPort();
-                InetAddress ipCliente = packet.getAddress();
-
-                mensaje = "Hola mundo desde el servidor";
-                buffer = mensaje.getBytes();
-
-                DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, ipCliente, puertoCliente);
-
-                System.out.println("Enviando respuesta al cliente con puerto " + puertoCliente + " e IP " + ipCliente);
-                socket.send(respuesta);
+            if (checksum != calculateChecksum(fileData)) {
+                System.out.println("Checksum incorrecto, descartando paquete " + seqNum);
+                sendAck(socket, receivePacket.getAddress(), receivePacket.getPort(), expectedSeq - 1);
+                continue;
             }
 
-        } catch (SocketException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            if (seqNum == expectedSeq) {
+                fos.write(fileData);
+                expectedSeq++;
+                if (isLast) lastPacketReceived = true;
+            }
+
+            sendAck(socket, receivePacket.getAddress(), receivePacket.getPort(), expectedSeq - 1);
         }
 
+        fos.close();
+        socket.close();
+        System.out.println("Archivo recibido exitosamente.");
     }
 
+    private static void sendAck(DatagramSocket socket, InetAddress address, int port, int seqNum) throws IOException {
+        byte[] ackData = new byte[]{(byte) (seqNum >> 8), (byte) seqNum};
+        DatagramPacket ackPacket = new DatagramPacket(ackData, ackData.length, address, port);
+        socket.send(ackPacket);
+        System.out.println("ACK enviado: " + seqNum);
+    }
+
+    private static byte calculateChecksum(byte[] data) {
+        byte checksum = 0;
+        for (byte b : data) checksum ^= b;
+        return checksum;
+    }
 }
